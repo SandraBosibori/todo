33e8405bd78722b63c9b4527a23da705882bdf56
SandraBosibori <sandrahbosibori@gmail.com>
2024-02-19 23:01:07 +0300

    second commit

---
+++ todo/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './App.css'
+
+const App = () => {
+    return (
+      <>
+        <div className={styles.full}>
+          <div className={styles.head}>
+            <h1>To Do List</h1>
+          </div>
+        </div>
+      </>
+    );
+};
+
+export default App;
